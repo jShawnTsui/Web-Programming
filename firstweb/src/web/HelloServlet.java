1ec0af2e72c06aa4cc2beff707a987fd8df0fa34
Shawn <tsuihsiangyang@gmail.com>
2018-06-15 17:19:00 -0400

    This is my first Java web application

---
+++ firstweb/src/web/HelloServlet.java
@@ -0,0 +1,19 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	public void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello Servlet</h1>");
+		out.close();
+	}
+}
