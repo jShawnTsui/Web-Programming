85f994a5416fd359791a19563c7bd31c24f35e9c
Shawn <tsuihsiangyang@gmail.com>
2018-07-26 16:40:17 -0400

    React02

--- JavaScript/ReactJS/react02/src/Person/Person.js
+++ JavaScript/ReactJS/react02/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
 	return (
-		<div>
+		<div className='Person'>
 			<h2 onClick={props.click}>I am a { props.name }, I am { props.age } years old. </h2>
 		    <p>{ props.children }</p>
 		    <input type="text" onChange={props.changed} />
