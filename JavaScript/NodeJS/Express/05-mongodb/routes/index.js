19514aa27a18ed0fca4f4662bd201e45614da971
Shawn <tsuihsiangyang@gmail.com>
2018-07-30 13:52:29 -0400

    MongoDB

--- JavaScript/NodeJS/Express/05-mongodb/routes/index.js
+++ JavaScript/NodeJS/Express/05-mongodb/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongo = require('mongodb');
 var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://localhost:27017';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,9 +11,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/get-data', function(req, res, next) {
-  mongo.connect(url, function(err, db) {
+  var resultArray = [];
+  mongo.connect(url, { useNewUrlParser: true }, function(err, client) {
     assert.equal(null, err);
-    var cursor = 
+    let db = client.db('test');
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      client.close();
+      res.render('index', {items: resultArray});
+    });
   });
 });
 
@@ -24,12 +33,16 @@ router.post('/insert', function(req, res, next) {
     author: req.body.author
   };
 
-  mongo.connect(url, function(err, db) {
+  mongo.connect(url, { useNewUrlParser: true }, function(err, client) {
     assert.equal(null, err);
-    db.collection('user-data').insertOne(item, function(err, ))
+    let db = client.db('test');
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      client.close();
+    });
   });
+  res.redirect('/');
 });
-
-
 
 module.exports = router;
