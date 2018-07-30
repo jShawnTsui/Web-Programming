d76c07ba75e798e5aaf11680d5adeb93ba4d8587
Shawn <tsuihsiangyang@gmail.com>
2018-07-30 13:00:58 -0400

    MongoDB

--- JavaScript/NodeJS/Express/05-mongodb/routes/index.js
+++ JavaScript/NodeJS/Express/05-mongodb/routes/index.js
@@ -7,12 +7,12 @@ var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shawn', condition: true, anyArray: [1,2,3] });
+  res.render('index');
 });
 
-router.get();
+// router.get();
 
-router.post();
+// router.post();
 
 
 
